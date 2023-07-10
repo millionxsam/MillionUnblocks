8db002f3f689b793cd56f173b5583f484adfbccf
Millionxsam <samdas3742@gmail.com>
2023-07-10 11:14:52 -0400

    Update index.mjs

--- index.mjs
+++ index.mjs
@@ -1,21 +1,17 @@
-import Server from 'bare-server-node';
 import http from 'http';
 import nodeStatic from 'node-static';
 
 
-const bare = new Server('/bare/', '');
 const serve = new nodeStatic.Server('static/');
 const port = 3000;
 
 const server = http.createServer();
 
 server.on('request', (request, response) => {
-  if (bare.route_request(request, response)) return true;
   serve.serve(request, response);
 });
 
 server.on('upgrade', (req, socket, head) => {
-  if (bare.route_upgrade(req, socket, head)) return;
   socket.end();
 });
 
